9516517e811247019a22b5923a7f43ad0388d867
Mussa Mussa <Mussa Mussa>
2021-09-19 10:55:30 +0100

    navbar component added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import '../App.css';
+import Navbar from './Navbar/Navbar';
 import Hero from './Hero/Hero'
 
 function App() {
   return (
     <div className="app--container">
       <div className="container">
+        <Navbar />
         <Hero />
       </div>
     </div>
